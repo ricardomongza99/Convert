4e245bb4f3c999f1a51cd85d085121f7658a749f
Ricardo <ricardomongza@gmail.com>
2022-02-16 11:54:52 -0600

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Ricardo Montemayor on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
