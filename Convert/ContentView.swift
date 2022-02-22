79a17a6112c5a62703912856df4bbb8a4cafbada
Ricardo <ricardomongza@gmail.com>
2022-02-22 10:55:16 -0600

    Made ConverterView

--- Convert/ContentView.swift
+++ Convert/ContentView.swift
@@ -8,13 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var fromValue = 0.0
-    private var toValue: Double {
-        let temp = Measurement(value: fromValue, unit: fromUnit.unit)
-        return temp.converted(to: toUnit.unit).value.rounded(4)
-    }
-    
+
     @State private var units = ModelData().massUnits
 
     @State private var fromUnit = ModelData().massUnits[1]
@@ -24,59 +18,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
-            Section {
-                VStack {
-                    HStack(alignment: .center, spacing: 20){
-                        TextField("", value: $fromValue, format: .number)
-                            .multilineTextAlignment(.center)
-                            .font(.title)
-                            .frame(height: 64)
-                            .padding([.leading, .trailing], 8)
-                            .background(
-                                Capsule().fill(Color.white)
-                            )
-                            .keyboardType(.decimalPad)
-                            
-                        Text("=")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            .bold()
-
-                        Text(toValue, format: .number)
-                            .font(.title)
-                            .frame(height: 64)
-                            .frame(maxWidth: .infinity)
-                            .padding([.leading, .trailing], 8)
-                            .background(
-                                Capsule().fill(Color.white)
-                            )
-
-                    }
-                    
-                    HStack(alignment: .top, spacing: 0) {
-                        Text(fromUnit.symbol)
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-
-                        Button {
-                            
-                        } label: {
-                            Image("arrows")
-                                .aspectRatio(1, contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                        }
-
-                        Text(toUnit.symbol)
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                    }
-                }
-                .padding()
-
-            }
-            .background(Color.primaryColor)
+            
+            ConverterView(fromUnit: $fromUnit, toUnit: $toUnit)
             
             VStack(spacing: 20) {
                 HStack(spacing: 20) {
--- Convert/Views/ConverterView.swift
+++ Convert/Views/ConverterView.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct ConverterView: View {
+    
+    @Binding var fromUnit: Unit
+    @Binding var toUnit: Unit
+    
+    @State private var fromValue = 0.0
+    private var toValue: Double {
+        let temp = Measurement(value: fromValue, unit: fromUnit.unit)
+        return temp.converted(to: toUnit.unit).value.rounded(4)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Section {
+            VStack {
+                HStack(alignment: .center, spacing: 20){
+                    TextField("", value: $fromValue, format: .number)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .frame(height: 64)
+                        .padding([.leading, .trailing], 8)
+                        .background(
+                            Capsule().fill(Color.white)
+                        )
+                        .keyboardType(.decimalPad)
+                        
+                    Text("=")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .bold()
+
+                    Text(toValue, format: .number)
+                        .font(.title)
+                        .frame(height: 64)
+                        .frame(maxWidth: .infinity)
+                        .padding([.leading, .trailing], 8)
+                        .background(
+                            Capsule().fill(Color.white)
+                        )
+
+                }
+                
+                HStack(alignment: .top, spacing: 0) {
+                    Text(fromUnit.symbol)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+
+                    Button {
+                        
+                    } label: {
+                        Image("arrows")
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                    }
+
+                    Text(toUnit.symbol)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .padding()
+
+        }
+        .background(Color.primaryColor)
     }
 }
 
 struct ConverterView_Previews: PreviewProvider {
+    static let massUnits = ModelData().massUnits
     static var previews: some View {
-        ConverterView()
+        ConverterView(
+            fromUnit: .constant(massUnits[0]),
+            toUnit: .constant(massUnits[4]))
     }
 }
