3e3edbadd80144a5ebf743f61cf4a9f795521839
Ricardo <ricardomongza@gmail.com>
2022-03-27 18:15:02 -0600

    Encapsulated `ConverterView` views

--- Convert/Views/Components/ConverterView.swift
+++ Convert/Views/Components/ConverterView.swift
@@ -13,48 +13,47 @@ struct ConverterView: View {
     var body: some View {
         VStack {
             HStack(alignment: .center, spacing: 20) {
-                TextField("", text: $viewModel.fromValue)
-                    .multilineTextAlignment(.center)
-                    .font(.title)
-                    .frame(height: 64)
-                    .padding([.leading, .trailing], 8)
-                    .background(
-                        Capsule().fill(Color.lightGray)
-                    )
-                    .keyboardType(.decimalPad)
-                    
+                inputTextField(text: $viewModel.fromValue)
                 Text("=")
                     .font(.title)
-
-                Text(viewModel.toValue, format: .number)
-                    .font(.title)
-                    .frame(height: 64)
-                    .frame(maxWidth: .infinity)
-                    .padding([.leading, .trailing], 8)
-                    .background(
-                        Capsule().fill(Color.lightGray)
-                    )
-
+                // TODO: replace with another input textField
+                outputText
             }
             
             HStack(alignment: .top, spacing: 0) {
                 unitText(text: viewModel.fromUnit.unit.symbol)
-
-                Button {
-                    viewModel.swapUnits()
-                } label: {
-                    Image("ArrowsIcon")
-                        .resizable()
-                        .renderingMode(.template)
-                        .foregroundColor(.primaryColor)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                }
-
+                convertButton
                 unitText(text: viewModel.toUnit.unit.symbol)
             }
         }
         .padding()
+    }
+    
+    // MARK: - COMPONENTS
+    
+    @ViewBuilder
+    private func inputTextField(text: Binding<String>) -> some View {
+        TextField("", text: text)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .frame(height: 64)
+            .padding([.leading, .trailing], 8)
+            .background(
+                Capsule().fill(Color.lightGray)
+            )
+            .keyboardType(.decimalPad)
+    }
+    
+    @ViewBuilder
+    private var outputText: some View {
+        Text(viewModel.toValue, format: .number)
+            .font(.title)
+            .frame(height: 64)
+            .frame(maxWidth: .infinity)
+            .padding([.leading, .trailing], 8)
+            .background(
+                Capsule().fill(Color.lightGray)
+            )
     }
     
     @ViewBuilder
@@ -63,6 +62,20 @@ struct ConverterView: View {
             .font(.title)
             .foregroundColor(.black)
             .frame(maxWidth: .infinity)
+    }
+    
+    @ViewBuilder
+    private var convertButton: some View {
+        Button {
+            viewModel.swapUnits()
+        } label: {
+            Image("ArrowsIcon")
+                .resizable()
+                .renderingMode(.template)
+                .foregroundColor(.primaryColor)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+        }
     }
 }
 
